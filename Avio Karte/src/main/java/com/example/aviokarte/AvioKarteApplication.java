fc4d4eb769ead892a6e34655808d133b6176c50a
Srdjan <srdjannn321@gmail.com>
2024-11-02 22:51:17 +0100

    first commit

---
+++ Avio Karte/src/main/java/com/example/aviokarte/AvioKarteApplication.java
@@ -0,0 +1,13 @@
+package com.example.aviokarte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvioKarteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AvioKarteApplication.class, args);
+    }
+
+}
